e7104424e2726a5d7be1ab09391d80cfd964ed5b
ayssegull <80278045+ayssegull@users.noreply.github.com>
2021-03-11 17:16:51 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,34 @@
-#Explain your work
+#CV application
+#Create 5 dictionaries. Each dictionary should represent a CV.
+#Create a CV containing the information of the 5 created people.
+#Print the information on CVs created on the screen.
 
 #Question 1
-for x in range(a):
-	print(a)
+CV1 = {"Ad":"Tuğra", "Soyad":"Ergin", "Yaş":20, "Meslek":"Bilgisayar Mühendisi", "Yetenekler":["Python","C++","C#"]}
+CV2 = {"Ad":"Neriman", "Soyad":"Demir", "Yaş":25, "Meslek":"Endüstri Mühendisi", "Yetenekler":["Programlama","Organizasyon"]}
+CV3 = {"Ad":"Ayşegül", "Soyad":"Çelik", "Yaş":21, "Meslek":"Makine Mühendisi", "Yetenekler":["Otomotiv","AutoCad","Solidworks"]}
+CV4 = {"Ad":"Yusuf", "Soyad":"Çılgın", "Yaş":22, "Meslek":"Kimya Mühendisi", "Yetenekler":["Laboratuvar","GMP-GLP"]}
+CV5 = {"Ad":"Aziz", "Soyad":"Ergin", "Yaş":24, "Meslek":"Elektrik Mühendisi", "Yetenekler":["Devreler","Aurdino","Boks"]}
+
+for i,j in CV1.items():
+  print(i, ":", j)
+
+print("\n")
+
+for i,j in CV2.items():
+  print(i, ":", j)
+
+print("\n")
+
+for i,j in CV3.items():
+  print(i, ":", j)
+
+print("\n")
+
+for i,j in CV4.items():
+  print(i, ":", j)
+
+print("\n")
+
+for i,j in CV5.items():
+  print(i, ":", j)
